36caca824884c60f2348758453cb9a0332361e3d
aryamohan17 <aryamohan0194@gmail.com>
2023-08-08 14:56:46 +0530

    first commit

--- cntrl/mongoCntrl.js
+++ cntrl/mongoCntrl.js
@@ -1,26 +1,57 @@
 var model_data = require('../model/mongoModel')
 
-function addStud(req,res,next){
-    var result_ ={
-        stud_name:req.body.stud_name,
-        stud_address:req.body.stud_address,
-        stud_age:req.body.stud_age
+// insert data
+
+function addStud(req, res, next) {
+    var result_ = {
+        stud_name: req.body.stud_name,
+        stud_address: req.body.stud_address,
+        stud_age: req.body.stud_age
     }
     var result = model_data.User.create(result_)
     res.locals.result = result
     next()
-    
+
 }
-    // return model_data.find().then(result=>{
-    //     if(result){
-    //         { 
-    //             model_data.save()             
-    //             // const newStud=new model_data.User({stud_name , stud_address , stud_age})
-    //             // newStud.save()            
-    //         }
-    //     }
-       
-    // })
 
+// update data
+async function updateStud(req, res, next) {
 
-module.exports={addStud}
+    const stud_id_ = req.params._id
+    const stud_id = { _id: stud_id_ }
+    const update_data = req.body.data
+    var result = await model_data.User.updateOne(stud_id, update_data)
+    res.locals.result = result
+    next()
+}
+ 
+// select
+
+async function selectAll(req,res,next) {
+    var result = await model_data.User.find().exec()
+    console.log(result);
+    res.locals.result = result
+    next()
+}
+// select by using one id 
+
+async function selectById(req,res,next) {
+    var stud_id = req.params._id
+    var stud_id_ = {_id:stud_id}
+   var result = await model_data.User.findOne(stud_id_)
+
+   res.locals.result = result
+
+   next()
+}
+
+async function delete_one(req,res,next) {
+   var stud_id_ = req.params._id
+    // console.log(stud_id_);
+    var result = await model_data.User.deleteOne(stud_id_)
+
+    res.locals.result = result
+    next()
+}
+
+module.exports = { addStud, updateStud ,selectAll,selectById,delete_one}
--- routes/mongoDemo.js
+++ routes/mongoDemo.js
@@ -18,4 +18,61 @@ router.post('/insert_data',cntrl.addStud,function (req,res,next) {
      }
 })
 
+router.put('/updateStud/:_id',cntrl.updateStud,function (req,res,next) {
+    if(res.locals.result == 'err' )
+    {
+        var status = {'status' : false,'message' :"Something went wrong",result: [] }
+            res.status(402).json(status);
+    }
+    else if(res.locals.result.length == 0){
+        var status = {'status' : true,'message' :"No result Found",result: [] }
+            res.status(202).json(status);
+    }
+    else{
+        var status = {'status' : true,'message' :"succesfull response",result: res.locals.result }
+            res.status(200).json(status);
+    }
+})
+
+router.get('/getAllData',cntrl.selectAll,function (req,res,next) {
+    console.log(res.locals.result);
+    if(res.locals.result == 'err'){
+        var status = {'status':false,'message':"something error",result:[]}
+        res.status(402).json(status)
+    }else if(res.locals.result.length == 0){
+        var status = {'status':true , 'message':"no found",result:[]}
+        res.status(202).json(status)
+    }else{
+        var status = {'status':true , 'message':"sucessfully get data",result:res.locals.result}
+        res.status(200).json(status);
+    }
+})
+
+router.get('/getById/:_id',cntrl.selectById,function (req,res,next) {
+    console.log(res.locals.result);
+    if(res.locals.result == 'err'){
+        var status = {'status':false , 'message':"some error" , result:[]}
+        res.status(400).json(status)
+    }else if(res.locals.result.length == 0){
+        var status = {'status':true , 'message':"not found",result:[]}
+        res.status(403).json(status)
+    }else{
+        var status = {'status':true,'message':"sucessfully get data",result:res.locals.result}
+        res.status(200).json(status)
+    }
+})
+
+router.delete('/delete_one/:id',cntrl.delete_one,function (req,res,next) {
+    if(res.locals.result == 'err'){
+        var status = {'status':false , 'message':"some error" , result:[]}
+        res.status(400).json(status)
+    }else if(res.locals.result.length == 0){
+        var status ={'status':true,'message':"not found",result:[]}
+        res.status(404).json(status)
+    }else{
+        var status ={'status':true , 'message':"delete sucessfully" , result:res.locals.result}
+        res.status(200).json(status)
+    }
+})
+
 module.exports=router
